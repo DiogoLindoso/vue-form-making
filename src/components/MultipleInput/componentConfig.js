dbefb3972506807cc0789dab922ced53df27ab25
Matheus Souza <m7hsouza@gmail.com>
2023-02-06 09:18:44 -0400

    adicionando configurações do multiple-input

---
+++ src/components/MultipleInput/componentConfig.js
@@ -0,0 +1,11 @@
+export default {
+  type: 'multipleinput',
+  icon: 'icon-icon_clone',
+  columns: [
+    { width: 'auto', item: null, },
+    { width: 'auto', item: null, }
+  ],
+  options: {
+    defaultValue: []
+  }
+}
